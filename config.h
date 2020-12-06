257b8ad27a1cbdfcc68c0468ecabf0945f8f1dfe
yunjie526 <a1351090507@163.com>
2020-12-06 11:15:10 +0800

    Change theme color

--- config.h
+++ config.h
@@ -10,8 +10,8 @@ static const char dmenufont[]       = "Noto Sans CJK:size=10";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char col_gray4[]       = "#ffffff";
+static const char col_cyan[]        = "#37474F";
 static const unsigned int baralpha = 0xd0;
 static const unsigned int borderalpha = OPAQUE;
 static const char *colors[][3]      = {
