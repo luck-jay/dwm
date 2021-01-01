8c72f33555c20d0c05f5e3efd20ee1afbc878622
yunjie526 <a1351090507@163.com>
2021-01-01 18:58:03 +0800

    删除一个快捷键

--- config.h
+++ config.h
@@ -101,7 +101,6 @@ static Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
 	{ MODKEY,                       XK_s,      show,           {0} },
-	{ MODKEY,                       XK_h,      hide,           {0} },
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
