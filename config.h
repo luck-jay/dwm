06d3b99698b9d917619b3a76f00982dca5006834
yunjie526 <a1351090507@163.com>
2021-03-07 15:44:02 +0800

    Some simple modifications

--- config.h
+++ config.h
@@ -6,7 +6,7 @@ static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 0;        /* 0 means bottom bar */
 static const char *fonts[]          = { "Source Code Pro:size=13" };
-static const char dmenufont[]       = "Source Code Pro:size=10";
+static const char dmenufont[]       = "Source Code Pro:size=13";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
@@ -64,14 +64,15 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "rofi", "-show", "run", NULL };
-static const char *termcmd[]  = { "alacritty", NULL };
-static const char *flameshot[]  = { "flameshot", NULL };
-static const char *volumed[]  = { "/home/pyj/sh/volumeDowm.sh", NULL };
-static const char *volumeu[]  = { "/home/pyj/sh/volumeUp.sh", NULL };
+//static const char *dmenucmd[]      = { "rofi", "-show", "run", NULL };
+static const char *dmenucmd[]      = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *termcmd[]       = { "alacritty", NULL };
+static const char *flameshot[]     = { "flameshot", NULL };
+static const char *volumed[]       = { "/home/pyj/sh/volumeDowm.sh", NULL };
+static const char *volumeu[]       = { "/home/pyj/sh/volumeUp.sh", NULL };
 static const char scratchpadname[] = "scratchpad";
 static const char *scratchpadcmd[] = { "st", "-t", scratchpadname, "-g", "120x34", NULL };
-static const char *trayer[] = { "/home/pyj/sh/showstatus.sh" ,NULL };
+static const char *trayer[]        = { "/home/pyj/sh/showstatus.sh" ,NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
